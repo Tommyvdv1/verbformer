a69a0a575c54d1aac448bbbf7698677e6fb71a32
Tommy Van Der Vaart <tommyvdv@hotmail.nl>
2019-11-15 22:58:03 +0100

    Updates

---
+++ precache-manifest.a1b5e52e9217184abae8a07ad6d5e925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacf6a3e552a422d47c65c5cd5941498",
+    "url": "/Verbformer/index.html"
+  },
+  {
+    "revision": "9d306a9fd0ce05c812f1",
+    "url": "/Verbformer/static/css/main.708ab4ee.chunk.css"
+  },
+  {
+    "revision": "791e4d8665895c40d6cb",
+    "url": "/Verbformer/static/js/2.ef2b8e5a.chunk.js"
+  },
+  {
+    "revision": "9d306a9fd0ce05c812f1",
+    "url": "/Verbformer/static/js/main.5e610ced.chunk.js"
+  },
+  {
+    "revision": "c1dd68ee8e4011909a7f",
+    "url": "/Verbformer/static/js/runtime-main.82f7381e.js"
+  }
+]);
